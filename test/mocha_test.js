375f554c5525914f54a2e4438ccf6aac885c401b
ハトネコエ <hato.nekoe@gmail.com>
2016-08-20 10:09:57 +0900

    テストコードの内容をアップデート

--- test/mocha_test.js
+++ test/mocha_test.js
@@ -1,10 +1,20 @@
-var assert = require('chai').assert;
+// var assert = require('chai').assert;
+var assert = require('assert'); // chai がなくても基本的な assert テストはできる (expect 系には chai 必要)
+
+describe('Assert', function() {
+	describe('#equal()', function () {
+		it('与えられた値が等しいか', function (){
+			assert.equal(2, 2);
+			assert.equal(-1, -1);
+		});
+	});
+});
 
 describe('Array', function() {
 	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function (){
-			assert.equal(-1, 2);
-			assert.equal(-1, -1);
+		it('配列に存在しない値を指定されたとき-1を返す', function (){
+			var arr = [3, 5];
+			assert.equal(-1, arr.indexOf(100));
 		});
 	});
 });
